4e9a494dc5defe997f92bc68e87e55b11993c22a
Beirão <phsicopt@gmail.com>
2024-06-12 09:35:26 +0100

    Added Minilibx

---
+++ Files/test.c
@@ -0,0 +1,17 @@
+#include <mlx.h>
+int main(void)
+{
+    void *mlx;
+
+    mlx = mlx_init();
+    if (!mlx)
+    {
+    // Handle the error.
+        return (1);
+    }
+    // Do some cool graphical stuff here…
+    // Clean up before exiting. The Display struct and the xvar struct
+    mlx_destroy_display(mlx);
+    free(mlx);
+    return (0);
+}
